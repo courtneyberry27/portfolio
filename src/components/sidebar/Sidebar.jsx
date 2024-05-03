ddfbe48fa0e68c65c798507ff9fb109f94d19d12
Courtney Berry <courtneyberry@Courtneys-Mac-mini.local>
2024-05-03 17:18:34 -0500

    testing push

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './sidebar.css';
+import Logo from "../../assets/logo.svg";
+
+const Sidebar = () => {
+  return (
+    <aside className='aside'>
+      <a href="#home" className='nav__logo'>
+        <img src={Logo} alt='' />
+      </a>
+
+      <nav className='nav'>
+        <div className='nav__menu'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href="#home" className='nav__link'>
+                <i className='icon-home'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href="#about" className='nav__link'>
+                <i className='icon-eyeglass'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href="#resume" className='nav__link'>
+                <i className='icon-briefcase'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href="portfolio" className='nav__link'>
+                <i className='icon-notebook'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href="contact" className='nav__link'>
+                <i className='icon-paper-plane'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className='nav__footer'>
+        <i className='icon-emotsmile'></i>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
